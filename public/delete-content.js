83e3112952142935455b76fd1699bd3a8dc9c69e
Matheus Alves Pereira <matttalves@gmail.com>
2023-02-21 23:51:22 -0300

    feat: modal injection pop

---
+++ public/delete-content.js
@@ -0,0 +1,5 @@
+(function deleteIframe() {
+  const iframeId = 'redirect-switch-modal'
+  const iframeAlreadyExist = document.getElementById(iframeId)
+  iframeAlreadyExist && iframeAlreadyExist.remove()
+})()
